b47f9accf9dd882d8025ea35645f61f187143396
Eurico M. <eurico.dp.magalhaes@outlook.com>
2025-10-18 12:58:39 +0100

    Update catasRearm.cs

--- catasRearm.cs
+++ catasRearm.cs
@@ -6,8 +6,8 @@ public void Save()
 {
 }
 
-public int sumItems(string itemName, string itemTypeName, IMyInventory inventory) {
-    
+// Sum all items with given itemName and itemTypeName that are present in an inventory
+public int sumItems(string itemName, string itemTypeName, IMyInventory inventory) {    
     MyFixedPoint fixedPoint;
     MyItemType itemType = new MyItemType(itemTypeName, itemName);
     fixedPoint = inventory.GetItemAmount(itemType);
@@ -15,6 +15,7 @@ public int sumItems(string itemName, string itemTypeName, IMyInventory inventory
     return intValue;
 }
 
+// iterates through containers, summing the ammount of a specific given itemName
 public int currentAmmo(string[] containerNames, string itemName) {
     int currentAmmoCount = 0;
     string itemType = "MyObjectBuilder_AmmoMagazine";
@@ -22,16 +23,30 @@ public int currentAmmo(string[] containerNames, string itemName) {
     foreach (string containerName in containerNames) {
         
         var container = GridTerminalSystem.GetBlockWithName(containerName) as IMyCargoContainer;
-        int numberInventories = blockName.InventoryCount;
+        int numberInventories = container.InventoryCount;
 
-        for (int i = 0; i < numberInventories; i++) {            
-            IMyInventory inventory = blockName.GetInventory(i);
+        for (int i = 0; i < numberInventories; i++) {
+            IMyInventory inventory = container.GetInventory(i);
             int itemCount = sumItems(itemName, itemType, inventory);
             currentAmmoCount += itemCount;
         }
     };
 
     return currentAmmoCount;
+}
+
+public class AmmoType {
+    public string name;
+    public string[] containerNames;
+    public int desiredAmount;
+    public MyFixedPoint missingAmount;
+
+    public AmmoType(string nm, string[] cNms, int dAmount) {
+        name = nm;
+        containerNames = cNms;
+        desiredAmount = dAmount;
+        missingAmount = 0;
+    }
 }
 
 public void Main(string argument, UpdateType updateSource) {
@@ -50,16 +65,31 @@ public void Main(string argument, UpdateType updateSource) {
         "container 4 Name"
     };
 
-    Dictionary<string,int> requiredAmmoAmounts = {
-        {ammoTypeA_Name, 100},
-        {ammoTypeB_Name, 100}
-    }
+    int ammoTypeA_desired = 100;
+    int ammoTypeB_desired = 100;
+
+    string sourceContainerName = "source container name";
+
     // STOP SETTING
     
+    AmmoType railgunAmmo = new AmmoType(ammoTypeA_Name, ammoTypeA_ContainerNames, ammoTypeA_desired);
+    AmmoType cannonAmmo = new AmmoType(ammoTypeB_Name, ammoTypeB_ContainerNames, ammoTypeB_desired);
 
-    int ammoTypeA_current = currentAmmo(ammoTypeA_ContainerNames, ammoTypeA_Name);
-    int ammoTypeB_current = currentAmmo(ammoTypeB_ContainerNames, ammoTypeB_Name);
+    List<AmmoType> ammoTypes = new List<AmmoType> {
+        railgunAmmo,
+        cannonAmmo
+    };
 
+    foreach (var ammo in ammoTypes) {
+        ammo.missingAmount = ammo.desiredAmount - currentAmmo(ammo.containerNames, ammo.name);
+    }
+
+    foreach (var ammo in ammoTypes) {
+        if (ammo.missingAmount > 0) {
+            var sourceContainer = GridTerminalSystem.GetBlockWithName(sourceContainerName) as IMyCargoContainer;
+            var inventory = 
+        }
+    }
     
 
     
